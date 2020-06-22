5696e11d5634dd42716880184d2e627ae97a267c
warren <hwhong1129@gmail.com>
2020-06-21 18:06:58 -0700

    c

---
+++ c/memory.h
@@ -0,0 +1,13 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(previous, type, oldCount, count) \
+    (type*)reallocate(previous, sizeof(type) * (oldCount), \
+        sizeof(type) * (count))
+
+void* reallocate(void* previous, size_t oldSize, size_t newSize);
+
+#endif
